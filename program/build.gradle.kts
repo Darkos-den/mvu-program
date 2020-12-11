9766a3929885f49a614c094d3b82015a2931d69b
Darkos <loboda.deni@gmail.com>
2020-12-11 16:35:26 +0200

    update version

--- program/build.gradle.kts
+++ program/build.gradle.kts
@@ -11,7 +11,7 @@ val repository = "MVU"
 
 val artifactName = "program"
 val artifactGroup = "com.$organization.$repository"
-val artifactVersion = "0.1.0"
+val artifactVersion = "0.1.1"
 
 group = artifactGroup
 version = artifactVersion
